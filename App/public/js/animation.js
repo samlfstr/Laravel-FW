aa8cf97023c7023615b4d8a0baa85ae75ca229e0
Aaron <samlfstr@gmail.com>
2021-09-13 21:38:45 +0300

    update

--- App/public/js/animation.js
+++ App/public/js/animation.js
@@ -1,11 +1,34 @@
+let btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) =>{
+console.log(btn);
+});
+
+
 let orginal_loc = 0;
+// call the function move to right every ten sec.
 let interval = setInterval(moveToRight, 10);
 
+// increase the location var by 1 assigne it to the style parameter
 function moveToRight(){
   orginal_loc++;
   document.getElementById("circle").style.left = orginal_loc + "px";
 }
 
+// stop calling the interval callback function after 5 sec.
 setTimeout(function(){
   clearInterval(interval);
 },5000);
+
+if(event.keyCode == 37) {
+  alert('Left arrow of keyboard was smashed');
+}
+else if(event.keyCode == 38) {
+  alert('Up arrow of keyboard was smashed');
+}
+else if(event.keyCode == 39) {
+  alert('Right arrow of keyboard was smashed');
+}
+else if(event.keyCode == 40) {
+  alert('Down arrow of keyboard was smashed');
+}
--- App/resources/views/animate1.blade.php
+++ App/resources/views/animate1.blade.php
@@ -6,18 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
-<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300' rel='stylesheet' type='text/css'>
 
-<link rel="stylesheet" href="{{ URL::asset('css/animation.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/animation.css') }}">
   </head>
 
   <body>
 
     <div class="container">
       <div id="circle"></div>
+
+      <div class="btn-group">
+        <button type="button" class="btn left-btn" id="left_btn">←</button>
+        <button type="button" class="btn right-btn" id="right_btn">→</button>
+        <button type="button" class="btn up-btn" id="up_btn">↑</button>
+        <button type="button" class="btn down-btn" id="down_btn">↓</button>
+      </div>
     </div>
+
+
 
 
 
