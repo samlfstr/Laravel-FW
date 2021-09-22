b58b150dcdf474ab7a6dc257eee7a129a653a240
Aaron <samlfstr@gmail.com>
2021-09-22 23:06:08 +0300

    update

--- App/public/js/modal.js
+++ App/public/js/modal.js
@@ -1,1 +1,17 @@
-console.log('Com succesful');
+let modal_btn = document.querySelector('.modal-btn');
+let modal_overlay = document.querySelector('.modal-overlay');
+let close_btn = document.querySelector('.close-btn');
+
+modal_btn.addEventListener(
+  'click',
+  ()=>{
+    modal_overlay.classList.add('open-modal');
+  }
+);
+
+close_btn.addEventListener(
+  'click',
+  ()=>{
+    modal_overlay.classList.remove('open-modal');
+  }
+);
--- App/resources/views/modal.blade.php
+++ App/resources/views/modal.blade.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{ URL::asset('css/modal.css') }}">
-  <title>Modal</title>
-</head>
-<body>
-  
 
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/modal.css') }}">
+    <link rel="stylesheet" href=" {{ mix ('css/app.css') }}">
+    <title>Modal</title>
+  </head>
 
-<script src="{{ URL::asset('js/modal.js') }}"></script> 
-</body>
+  <body>
+    <!-- Hero -->
+    <header class="hero">
+      <div class="banner">
+        <h1>Modal Project</h1>
+        <button class="btn modal-btn">
+          Open Modal
+        </button>
+      </div>
+    </header>
+    <!-- End of hero -->
+    <!-- Modal -->
+    <header class="modal-overlay">
+      <div class="modal-container">
+        <h1>Modal Content</h1>
+        <button class="close-btn">
+          <i class="fa fa-times"></i>
+        </button>
+      </div>
+    </header>
+    <!-- End of modal -->
+
+    <script src="{{ URL::asset('js/modal.js') }}"></script>
+  </body>
+
 </html>
