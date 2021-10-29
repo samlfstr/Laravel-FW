77559161fa179996b4dd721eea8f449f69151fdf
Aaron <samlfstr@gmail.com>
2021-10-29 17:08:44 +0300

    added middleware route group

--- App/routes/web.php
+++ App/routes/web.php
@@ -28,44 +28,53 @@ Route::get('/greeting', function () {
     return 'Hello world';
 });
 
-Route::permanentRedirect('/here','/to_there');
+Route::permanentRedirect('/here', '/to_there');
 
-Route::get('/exc', function () {
-    return view("routing")->with(["name" => "Taylor"]);
-});
-
-Route::get('/bg', function () {
-    return view('bg_color');
-});
-
-Route::get('/counter', function () {
-    return view('counter');
-});
-
-Route::get('/reviews', function () {
-    return view('reviews');
-});
-
-Route::get('/animate1', function () {
-    return view('animate1');
-});
-
-Route::get('/modal', function () {
-    return view('modal');
-});
-
-Route::get('/questions', function () {
-    return view('questions');
-});
-
-Route::get('/todo', function () {
-    return view('todo');
-});
-
-Route::get('/test', function () {
-    return view('test');
-});
 
 Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/userName', [UsersController::class, 'show']);
+
+Route::group(['middleware' => 'age'], function () {
+    Route::get('/userName', [UsersController::class, 'show']);
+
+    Route::get('/bg', function () {
+        return view('bg_color');
+    });
+
+    Route::get('/exc', function () {
+        return view("routing")->with(["name" => "Taylor"]);
+    });
+
+    Route::get('/bg', function () {
+        return view('bg_color');
+    });
+
+    Route::get('/counter', function () {
+        return view('counter');
+    });
+
+    Route::get('/reviews', function () {
+        return view('reviews');
+    });
+
+    Route::get('/animate1', function () {
+        return view('animate1');
+    });
+
+    Route::get('/modal', function () {
+        return view('modal');
+    });
+
+    Route::get('/questions', function () {
+        return view('questions');
+    });
+
+    Route::get('/todo', function () {
+        return view('todo');
+    });
+
+    Route::get('/test', function () {
+        return view('test');
+    });
+
+});
