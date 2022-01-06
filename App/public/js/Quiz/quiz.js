5e70804b5b3bb2e3b9e191f1e19f07a95d5c72f5
Oz. Isa <samlfstr@gmail.com>
2022-01-06 23:28:12 +0300

    update

--- App/public/js/Quiz/quiz.js
+++ App/public/js/Quiz/quiz.js
@@ -3,24 +3,31 @@ let _names = Array();
 let answers = Array();
 let check = [];
 let questions = document.querySelectorAll('.question');
+// select all the inputs that has a name parameter
 let inputs = document.querySelectorAll('input[name]');
 let submit = document.querySelector('#submit');
 
 
 inputs.forEach((item, key) => {
     if (inputs[key].name !== undefined && inputs[key].name !== null && inputs[key].name.length !== 0) {
+        // take all the names out of that query selection
         _names.push(inputs[key].name);
     }
+    // delete all duplicate elements
     let uniq = [...new Set(_names)];
 
-
+    // event delegation to detect each new selection
     item.addEventListener('click', () => {
+        // insert selected class to each selected radio button
         item.classList.value = "selected";
+        // then catch them
         if (item.classList.contains('selected')) {
             answers = [];
-
+            // for each name
             uniq.forEach((item, key) => {
+                // go through all the unique names
                 if (document.querySelector(`input[name="${uniq[key]}"]:checked`)) {
+                    // with each new user entry go through all the inputs over again
                     check = document.querySelectorAll(`input[name="${uniq[key]}"]:checked`);
                     answers.push(check[0].value);
                 }
@@ -30,23 +37,3 @@ inputs.forEach((item, key) => {
         }
     });
 });
-
-
-/*inputs.forEach((item, key) => {
-    if (inputs[key].name !== undefined && inputs[key].name !== null && inputs[key].name.length !== 0) {
-        _names.push(inputs[key].name);
-
-    }
-    if (key === inputs.length - 1) {
-        let uniq = [...new Set(_names)];
-        uniq.forEach((item, key) => {
-            if (document.querySelector(`input[name="${uniq[key]}"]:checked`)) {
-                answers.push(document.querySelector(`input[name="${uniq[key]}"]:checked`).value);
-            } else {
-                console.log('Not selected');
-            }
-        });
-
-    }
-
-});*/
