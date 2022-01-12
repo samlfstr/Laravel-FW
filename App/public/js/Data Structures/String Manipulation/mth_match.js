57bf1160ee3aa5511d0c7b9dcbf36cdb434add25
Oz. Isa <samlfstr@gmail.com>
2022-01-12 20:59:37 +0300

    update

--- App/public/js/Data Structures/String Manipulation/mth_match.js
+++ App/public/js/Data Structures/String Manipulation/mth_match.js
@@ -1,1 +1,13 @@
 console.log('mth_math.js');
+
+let str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi eveniet illo modi molestias perferendis provident quas sed tempore vitae? Consequatur consequuntur culpa dicta dolores exercitationem facilis, hic ipsum suscipit.";
+
+let query = 'dolor';
+let found = str.match(`${query}`);
+let index = found['index'];
+
+console.log(
+    'At the position : ' + index +
+    '\n' + 'The character : ' +
+    str.substring(index, index + found[0].length)
+);
